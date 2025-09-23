ec2ec3f5d8a062979a6f409d198bc4542e2e53e9
jellybean1402 <aayushkoul1402@gmail.com>
2025-09-23 21:24:29 +0530

    Setting up everything for CI

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,35 +1,3 @@
-import os
-import yaml
-import argparse
-import torchvision.datasets as datasets
-
-def data_collection(config_path):
-    """
-    Downloads the dataset specified in the params.yaml file.
-    Args:
-        config_path (str): Path to the params.yaml configuration file.
-    """
-    with open(config_path) as f:
-        config = yaml.safe_load(f)
-
-    dataset_name = config['dataset_name']
-    data_path = config['data_path']
-    raw_data_path = os.path.join(data_path, "raw")
-
-    if not os.path.exists(raw_data_path):
-        os.makedirs(raw_data_path)
-        print(f"Created directory: {raw_data_path}")
-
-    if dataset_name == "CIFAR10":
-        print(f"Downloading {dataset_name} dataset to {raw_data_path}...")
-        datasets.CIFAR10(root=raw_data_path, train=True, download=True)
-        datasets.CIFAR10(root=raw_data_path, train=False, download=True)
-        print("Download complete.")
-    else:
-        print(f"Dataset {dataset_name} is not supported.")
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", default="params.yaml")
-    args = parser.parse_args()
-    data_collection(config_path=args.config)
+version https://git-lfs.github.com/spec/v1
+oid sha256:5d25ca0389a3a2f1bcf6d2a2faeb2f6f4b8457bca2b998ddbe746c2127dbfe7c
+size 1162
---
+++ test.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fccd4834bab2dd13bc044f1330881af78a2eef5d7b06d4ad32de73d6cda48d43
+size 2419
